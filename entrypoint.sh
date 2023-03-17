f683ac74ed3b1919df7475e272e3a4eacd1ef6d4
Elijah Hood <eli@Elijahs-MacBook-Air.local>
2023-03-17 12:28:36 -0400

    updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# docker exec -it  fullstacktypescript_database_1 psql -U postgres -c "CREATE DATABASE elitypescript"
+npx docker exec -it  fullstacktypescript_database_1 psql -U postgres -c "CREATE DATABASE elitypescript;"
 yarn run build
 yarn run migrate
 yarn run server
--- models/commentreplies.ts
+++ models/commentreplies.ts
@@ -16,31 +16,31 @@ export interface CommentRepliesInstance {
 }
 
 export default function(sequelize: Sequelize) {
-  let CommentReplies = sequelize.define("CommentReplies", {
+  const CommentReplies = sequelize.define("CommentReplies", {
     replyBody: DataTypes.TEXT,
     userId: DataTypes.INTEGER,
     commentId: DataTypes.INTEGER,
-    postId: DataTypes.INTEGER
+    postId: DataTypes.INTEGER,
   }) as any;
 
   CommentReplies.associate = function(models) {
     CommentReplies.belongsTo(models.User, {
       as: "author",
       foreignKey: "userId",
-      onDelete: "CASCADE"
+      onDelete: "CASCADE",
     });
     CommentReplies.belongsTo(models.Post, {
       foreignKey: "postId",
       timestamps: false,
       onDelete: "CASCADE",
-      targetKey: "id"
+      targetKey: "id",
     });
     CommentReplies.belongsTo(models.Comments, {
       as: "commentReplies",
       foreignKey: "commentId",
       timestamps: false,
       onDelete: "CASCADE",
-      targetKey: "id"
+      targetKey: "id",
     });
   };
 
