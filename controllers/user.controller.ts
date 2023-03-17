16ed1d24c841196af6e2c3ff6d8437dbb03a55d5
Elijah Hood <eli@Elijahs-MacBook-Air.local>
2023-03-17 19:32:25 -0400

    update

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import nodemailer from "nodemailer";
-// import sgTransport from "nodemailer-sendgrid-transport";
 import models from "../models";
 import { Session } from "express-session";
 
