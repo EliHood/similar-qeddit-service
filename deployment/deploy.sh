7cac1dc840170aa9ce08ec162879be86b4456b75
visortelle <visortelle@gmail.com>
2023-03-21 19:03:00 +0100

    Fix build

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -33,7 +33,7 @@ mkdir -p ~/.ssh && echo "${VM_SSH_KEY}" >~/.ssh/vm_ssh_key && chmod 600 ~/.ssh/v
 
 echo "Running deployment commands on VM"
 
-vm_command_populate_env="source ${VM_GIT_ROOT}/deployment/config/populate.sh"
+vm_command_populate_env="cd ${VM_GIT_ROOT} && source ./deployment/config/populate.sh"
 vm_command_pull_latest_changes="cd $VM_GIT_ROOT && git reset --hard HEAD && git checkout $git_branch && git pull origin $git_branch"
 vm_command_restart_docker_compose="cd $VM_GIT_ROOT/deployment && docker-compose pull && make restart"
 vm_commands="${vm_command_populate_env} && ${vm_command_pull_latest_changes} && ${vm_command_restart_docker_compose}"
