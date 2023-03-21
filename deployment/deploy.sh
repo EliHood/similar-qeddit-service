84a405aa7fdcdc38b9d2dbd2faf2ac1734245368
visortelle <visortelle@gmail.com>
2023-03-21 19:14:11 +0100

    Debug build

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
+set -xeo pipefail
 
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $script_dir/config/config.env
 
 git_branch=$(git rev-parse --abbrev-ref HEAD)
+echo "Current branch: ${git_branch}"
 
 echo "Deploying the current branch \"${git_branch}\" to AWS"
 
@@ -36,7 +37,7 @@ echo "Running deployment commands on VM"
 vm_command_populate_env="cd ${VM_GIT_ROOT} && source ./deployment/config/populate.sh"
 vm_command_pull_latest_changes="cd $VM_GIT_ROOT && git reset --hard HEAD && git checkout $git_branch && git pull origin $git_branch"
 vm_command_restart_docker_compose="cd $VM_GIT_ROOT/deployment && docker-compose pull && make restart"
-vm_commands="${vm_command_populate_env} && ${vm_command_pull_latest_changes} && ${vm_command_restart_docker_compose}"
+vm_commands="set -x; ${vm_command_populate_env} && ${vm_command_pull_latest_changes} && ${vm_command_restart_docker_compose}"
 
 ssh -i ~/.ssh/vm_ssh_key -o "StrictHostKeyChecking=no" "${vm_host}" "$vm_commands"
 
