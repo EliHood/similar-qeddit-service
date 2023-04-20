11ea1fbcc261dd3deaf5052d87cc92427a1ff812
Elijah Hood <eli@Elijahs-MacBook-Air.local>
2023-04-20 19:05:02 -0400

    update

--- sockets/notificationService.ts
+++ sockets/notificationService.ts
@@ -7,7 +7,6 @@ export default {
     const { userId, title } = post;
     const commenterName = commenter.username;
     const body = `${commenterName} just commented on article: ${title}`;
-    console.log("checking for user ids", userId, commenterId, currentUser);
     if (currentUser !== userId) {
       await models.Notification.create({ userId, body, status: "unread" });
     }
@@ -15,24 +14,23 @@ export default {
       body,
       currentUser,
       userId,
-      commenterId
+      commenterId,
     };
     pusherConfig.trigger("notification", "my-event", data);
   },
 
-  async userMention(currentUser, postId, mentionedUser, commenterId) {
+  async userMention(currentUser, postId, commenterId) {
     const post = await models.Post.findOne({ where: { id: postId } });
     const commenter = await models.User.findOne({ where: { id: commenterId } });
-    const { userId, title } = post;
+    const { userId } = post;
     console.log("notification", userId);
     const commenterName = commenter.username;
     const body = `${commenterName} mentioned you`;
-    console.log("checking for user ids", userId, commenterId, currentUser);
 
     if (currentUser !== userId) {
       await models.Notification.create({ userId, body, status: "unread" });
     }
 
     pusherConfig.trigger("notification", "user-mention", body);
-  }
+  },
 };
